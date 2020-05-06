9c9d660a8e4e271eba0b2c876b4368c457538476
Jacek <jacek.dominik.koziej@gmail.com>
2020-05-06 17:25:38 +0200

    dodanie obslugi hardgame

--- .idea/script.js
+++ .idea/script.js
@@ -8,32 +8,42 @@ const points = document.getElementById('points')
 const timer = document.getElementById('timer')
 const question = document.getElementById('question')
 var answer = document.getElementById('answer')
+//aktualna liczba rozegranych gier
 let gamesNumber = 1
 startSimpleButton.addEventListener('click', startSimpleGame)
 startNormalButton.addEventListener('click', startNormalGame)
-
+startHardButton.addEventListener('click', startHardGame)
+//zmienna helper mówi o tym, który rodzaj gry należy wywołać w funkcji validate 1-simple, 2-normal, 3-hard
+let helper
+//całkowita liczba gier
+let gamesCount
 confirmButton.addEventListener('click', validate)
 let liczba1 = Math.floor(Math.random() * 9) + 1
 let liczba2 = Math.floor(Math.random() * 9) + 1
 let iloczyn = liczba1 * liczba2
 let punkty = 0
+//pozostały czas
 var downloadTimer
 
 function startSimpleGame() {
-    let timeleft = 15
+    document.getElementById("progressBar2").classList.add('hide')
+    document.getElementById("progressBar3").classList.add('hide')
+    helper = 1
+    gamesCount = 10
+    var timeleft = 15
     clearInterval(downloadTimer)
     downloadTimer = setInterval(function () {
         if (timeleft <= 0) {
             clearInterval(downloadTimer);
             alert("Czas minął")
-            if (gamesNumber == 10) {
+            if (gamesNumber == gamesCount) {
                 alert("Koniec gry.\nZdobyłeś: " + punkty + " punktów!")
                 window.location.reload(true)
             } else
                 gamesNumber++
             startSimpleGame()
         }
-        document.getElementById("progressBar").value = 15 - timeleft
+        document.getElementById("progressBar1").value = 15 - timeleft
         timeleft -= 1
     }, 1000)
     liczba1 = Math.floor(Math.random() * 9) + 1
@@ -46,20 +56,54 @@ function startSimpleGame() {
 }
 
 function startNormalGame() {
-    let timeleft = 15
+    helper = 2
+    gamesCount = 15
+    document.getElementById("progressBar1").classList.add('hide')
+    document.getElementById("progressBar3").classList.add('hide')
+    var timeleft = 10
     clearInterval(downloadTimer)
     downloadTimer = setInterval(function () {
         if (timeleft <= 0) {
             clearInterval(downloadTimer);
             alert("Czas minął")
-            if (gamesNumber == 10) {
+            if (gamesNumber == gamesCount) {
                 alert("Koniec gry.\nZdobyłeś: " + punkty + " punktów!")
                 window.location.reload(true)
             } else
                 gamesNumber++
             startNormalGame()
         }
-        document.getElementById("progressBar").value = 15 - timeleft
+        document.getElementById("progressBar2").value = 10 - timeleft
+        timeleft -= 1
+    }, 1000)
+    liczba1 = Math.floor(Math.random() * 9) + 1
+    liczba2 = Math.floor(Math.random() * 9) + 1
+    iloczyn = liczba2 * liczba1
+    hideStartMenu();
+    let question = document.getElementById('question')
+    question.innerText = "Ile to jest...\n\n" + liczba1 + " * " + liczba2
+    points.innerText = "Ilość punktów: " + punkty
+}
+
+function startHardGame() {
+    helper = 3
+    gamesCount = 20
+    document.getElementById("progressBar1").classList.add('hide')
+    document.getElementById("progressBar2").classList.add('hide')
+    var timeleft = 5
+    clearInterval(downloadTimer)
+    downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            alert("Czas minął")
+            if (gamesNumber == gamesCount) {
+                alert("Koniec gry.\nZdobyłeś: " + punkty + " punktów!")
+                window.location.reload(true)
+            } else
+                gamesNumber++
+            startHardGame()
+        }
+        document.getElementById("progressBar3").value = 5 - timeleft
         timeleft -= 1
     }, 1000)
     liczba1 = Math.floor(Math.random() * 9) + 1
@@ -73,10 +117,9 @@ function startNormalGame() {
 
 function validate() {
     var wynik = Number(answer.value)
-    if (gamesNumber == 10) {
+    if (gamesNumber == gamesCount) {
         if (wynik === iloczyn) {
             alert("Dobra odpowiedź")
-            punkty += 10
             points.innerText = "Ilość punktów: " + punkty
             alert("Koniec gry.\nZdobyłeś: " + punkty + " punktów!")
             window.location.reload(true);
@@ -97,7 +140,12 @@ function validate() {
         }
     }
     answer.value = ""
-    startSimpleGame()
+    if (helper == 1)
+        startSimpleGame()
+    else if (helper == 2)
+        startNormalGame()
+    else if (helper == 3)
+        startHardGame()
 }
 
 function hideStartMenu() {

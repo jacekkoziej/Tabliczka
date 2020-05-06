e683f6c0599bed21e845d7575af36d0f785cfa6e
Jacek Koziej <jacek.koziej@finanteq.com>
2020-05-06 15:45:50 +0200

    podpięcie gry do przycisku normalnej gry

--- .idea/script.js
+++ .idea/script.js
@@ -1,6 +1,7 @@
 const startSimpleButton = document.getElementById('start-simple-btn')
 const startNormalButton = document.getElementById('start-normal-btn')
 const startHardButton = document.getElementById('start-hard-btn')
+
 const questionContainer = document.getElementById('question-container')
 const confirmButton = document.getElementById('confirm-btn')
 const points = document.getElementById('points')
@@ -9,6 +10,8 @@ const question = document.getElementById('question')
 var answer = document.getElementById('answer')
 let gamesNumber = 1
 startSimpleButton.addEventListener('click', startSimpleGame)
+startNormalButton.addEventListener('click', startNormalGame)
+
 confirmButton.addEventListener('click', validate)
 let liczba1 = Math.floor(Math.random() * 9) + 1
 let liczba2 = Math.floor(Math.random() * 9) + 1
@@ -29,6 +32,32 @@ function startSimpleGame() {
             } else
                 gamesNumber++
             startSimpleGame()
+        }
+        document.getElementById("progressBar").value = 15 - timeleft
+        timeleft -= 1
+    }, 1000)
+    liczba1 = Math.floor(Math.random() * 9) + 1
+    liczba2 = Math.floor(Math.random() * 9) + 1
+    iloczyn = liczba2 * liczba1
+    hideStartMenu();
+    let question = document.getElementById('question')
+    question.innerText = "Ile to jest...\n\n" + liczba1 + " * " + liczba2
+    points.innerText = "Ilość punktów: " + punkty
+}
+
+function startNormalGame() {
+    let timeleft = 15
+    clearInterval(downloadTimer)
+    downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            alert("Czas minął")
+            if (gamesNumber == 10) {
+                alert("Koniec gry.\nZdobyłeś: " + punkty + " punktów!")
+                window.location.reload(true)
+            } else
+                gamesNumber++
+            startNormalGame()
         }
         document.getElementById("progressBar").value = 15 - timeleft
         timeleft -= 1
